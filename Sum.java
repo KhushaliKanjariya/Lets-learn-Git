28b57ab130a008427d1b3b0bda1062cd96a21487
Khushali Kanjariya <khushalikanjariya24@gmail.com>
2022-02-20 22:59:11 +0530

    Added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,7 @@ public class Sum {
     public static void main(String[] args) {
         int a = 5;
         int b = 6;
+        System.out.print("Sum is:");
         System.out.println(a + b);
 
     }
