49d337b050682bf96c1edd398c04fd4dcdc9fb0e
Khushali Kanjariya <khushalikanjariya24@gmail.com>
2022-02-27 13:00:59 +0530

    Changes made in diff.java

--- diff.java
+++ diff.java
@@ -2,5 +2,5 @@ public class diff {
     public static void main(String[] args) {
 	    System.out.println("Just print the message in diff java source file");
 	    System.out.println("Welcome");
-
+	    System.out.println("Again added new content");
 }:W
